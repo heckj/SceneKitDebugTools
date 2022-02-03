49cb55ac3f699327d11b8c87f869c13c36cebb2e
Joe Heck <heckj@mac.com>
2022-02-03 13:36:24 -0800

    adding explicit tvOS and watchOS SDK support minimums

--- Package.swift
+++ Package.swift
@@ -8,6 +8,8 @@ let package = Package(
     platforms: [
         .macOS(.v12),
         .iOS(.v15),
+        .tvOS(.v13),
+        .watchOS(.v6),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
