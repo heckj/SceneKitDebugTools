6f128fe05d14f45d5c25deac8bdfcb8ed025c20b
Joe Heck <heckj@mac.com>
2022-07-26 11:38:43 -0700

    iterating on doc build

--- docbuild.bash
+++ docbuild.bash
@@ -9,6 +9,10 @@ mkdir -p html
 rm -rf .build
 mkdir -p .build/symbol-graphs
 
+# Enables deterministic output
+# - useful when you're committing the results to host on github pages
+export DOCC_JSON_PRETTYPRINT=YES
+
 swift build --target SceneKitDebugTools \
 -Xswiftc -emit-symbol-graph \
 -Xswiftc -emit-symbol-graph-dir -Xswiftc .build/symbol-graphs
@@ -16,6 +20,7 @@ swift build --target SceneKitDebugTools \
 # cull the non-Lindenmayer specific builds from the symbol graph files
 rm -f .build/symbol-graphs/MeshGenerator*
 
+# run the analysis process to report on the docs status
 xcrun docc convert Sources/SceneKitDebugTools/Documentation.docc \
 --analyze \
 --fallback-display-name SceneKitDebugTools \
@@ -36,15 +41,6 @@ xcrun docc convert Sources/SceneKitDebugTools/Documentation.docc \
 #--hosting-base-path '/' \
 #--emit-digest
 
-#
-# Generate a list of all the identifiers for DocC curation
-#
-
-cat docs/linkable-entities.json| jq '.[].referenceURL' -r > all_identifiers.txt
-sort all_identifiers.txt | sed -e 's/doc:\/\/SceneKitDebugTools\/documentation\///g' \
-| sed -e 's/^/- ``/g' | sed -e 's/$/``/g' > all_symbols.txt
-
-
 # Swift package plugin for hosted content:
 #
 swift package \
@@ -56,5 +52,13 @@ swift package \
     --disable-indexing \
     --transform-for-static-hosting \
     --hosting-base-path 'SceneKitDebugTools'
+
+#
+# Generate a list of all the identifiers for DocC curation
+#
+
+cat docs/linkable-entities.json| jq '.[].referenceURL' -r > all_identifiers.txt
+sort all_identifiers.txt | sed -e 's/doc:\/\/SceneKitDebugTools\/documentation\///g' \
+| sed -e 's/^/- ``/g' | sed -e 's/$/``/g' > all_symbols.txt
 
 echo "Page will be available at https://heckj.github.io/SceneKitDebugTools/documentation/scenekitdebugtools/"
